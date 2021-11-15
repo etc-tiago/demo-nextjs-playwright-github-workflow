9543636f79e2b304e4059f18130a659089296b4e
Tiago Souza <tiago@etc.digital>
2021-11-14 22:48:46 -0300

    change reference

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/dist/client/router";
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
